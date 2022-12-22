d6a631c4cbd557e0cfdb1fbd25e83c8e57f3005e
manuelguerra1 <chingo2005@gmail.com>
2022-12-22 18:41:13 -0300

    agregue el contenido de la clase 18

--- visualCode/modulo4/clase17/app.js
+++ visualCode/modulo4/clase17/app.js
@@ -6,7 +6,7 @@ const app = express()
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
-app.listen(8080, () => {
+app.listen(3030, () => {
     console.log('Esto funciono de manera perfecta!');
 })
 
